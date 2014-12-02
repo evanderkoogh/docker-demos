05ae28f2f7c4c936022b3bf673aa73617c53765a
Erwin van der Koogh <info@erronis.nl>
2014-12-02 22:58:28 +1100

    Add java-tomcat hello_world

---
+++ hello_worlds/java-tomcat/webapps/hello/WEB-INF/classes/HelloWorld.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.http.*;
+
+public class HelloWorld extends HttpServlet { 
+  public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException,IOException {
+    response.setContentType("text/html");
+    PrintWriter pw = response.getWriter();
+    pw.println("<html>");
+    pw.println("<head><title>Hello World</title></title>");
+    pw.println("<body>");
+    pw.println("<h1>Hello World!</h1>");
+    pw.println("<h2>from Java on Tomcat</h2>");
+    pw.println("</body></html>");
+  }
+}
